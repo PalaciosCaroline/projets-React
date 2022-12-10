7388f18dbc7af91f115a5e429de34368185d180b
PalaciosCaroline <caroline2807@yahoo.fr>
2022-12-10 21:48:06 +0100

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,7 +4,7 @@ export default function Banner(){
     
     return(
         <div className="img_banner">
-                <img className="header_logo" src={img_banner} alt="logo de Kasa" />
+             <h1 className="titre_banner">Chez vous, partout et ailleurs</h1>
         </div>
     )
 }
