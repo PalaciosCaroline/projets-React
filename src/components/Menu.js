97279192a7344988e1d5d04da567584ec85f2ba8
PalaciosCaroline <caroline2807@yahoo.fr>
2022-12-10 10:58:55 +0100

    mise en place du menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,13 +1,14 @@
 import {NavLink} from 'react-router-dom';
+import logo from "./../logo.svg";
 
 export default function Menu(){
     
     return(
         <div className="menu">
+                <img className="header_logo" src={logo} alt="logo de Kasa" />
             <ul>
                 <li><NavLink to="/ " className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Home</NavLink></li>
-                <li><NavLink to="/add" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>Add Techno</NavLink></li>
-                <li><NavLink to="/list" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>All Technos</NavLink></li>
+                <li><NavLink to="/list" className={({ isActive }) => (isActive ? "activeLink" : undefined)}>A propos</NavLink></li>
             </ul>
         </div>
     )
