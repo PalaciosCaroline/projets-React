f9ae507c371e0c4e5964f646ac1f79e9284370f1
PalaciosCaroline <caroline2807@yahoo.fr>
2022-12-14 00:03:11 +0100

    lien erreur404

--- src/pages/Erreur404_module/Erreur404.jsx
+++ src/pages/Erreur404_module/Erreur404.jsx
@@ -7,7 +7,7 @@ export default function Erreur404(){
         <div className="erreur404">
             <h1 className="Erreur404_title">404</h1>
             <h2 className="Erreur404_text">Oups ! La page que vous demandez n'existe pas.</h2>
-            <NavLink to="/Home" className="erreur404_link">Retourner sur la page d'accueil</NavLink>            
+            <NavLink to="/" className="erreur404_link">Retourner sur la page d'accueil</NavLink>            
         </div>
     )
 }
