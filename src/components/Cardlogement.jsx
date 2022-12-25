f0fe776e0c6f4d2abdd22e9e705447b2cb540e92
PalaciosCaroline <caroline2807@yahoo.fr>
2022-12-25 16:02:39 +0100

    Accessibilité des cardLogements

--- src/components/Cardlogement.jsx
+++ src/components/Cardlogement.jsx
@@ -4,7 +4,7 @@ import {NavLink} from 'react-router-dom';
 export default function Cardlogement({logement}) {
 
     return(
-        <NavLink to={"/logement/"+logement.id} className="link_logement" aria-label="lien vers la page du logement">
+        <NavLink to={"/logement/"+logement.id} className="link_logement" aria-label={`lien vers la page du logement ${logement.title}`}>
             <div className="card_logement" style={{backgroundImage:`url(${logement.cover})`}}>
                 <div className='card_filter'></div>
                 <h2>{logement.title}</h2>
