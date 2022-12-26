618c0c989dccb02cf09a3b5a662423f93afd19d8
PalaciosCaroline <caroline2807@yahoo.fr>
2022-12-26 10:46:38 +0100

    Nettoyage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,7 +34,7 @@ export default function Home() {
       {isDataLoading ? (
         <Loader />
         ) : (
-      <Boxcards logements={logements} />
+        <Boxcards logements={logements} />
       )}
     </main>
   );
