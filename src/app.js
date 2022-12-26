13bda9d99bdf7e71a4051cac2003dbf2a082b56c
PalaciosCaroline <caroline2807@yahoo.fr>
2022-12-26 16:02:03 +0100

    Configure React app for deployment to GitHub Pages

--- src/app.js
+++ src/app.js
@@ -5,25 +5,25 @@ import Menu from './components/Menu'
 import { useState, useEffect } from 'react'
 
 export default function App() {
-  // const [loader, setLoader] = useState(true);
+  const [loader, setLoader] = useState(true);
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setLoader(false);
-  //   }, 1500);
-  // }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 1500);
+  }, []);
 
   return (
-    // loader ? (
-    //   <div className="loader_appli">
-    //   <Loader />
-    //   </div>
-    // ) : (
+    loader ? (
+      <div className="loader_appli">
+      <Loader />
+      </div>
+    ) : (
     <>
       <Menu />
       <Router />
       <Footer />
     </>
-    // )
+    )
   )
 }
