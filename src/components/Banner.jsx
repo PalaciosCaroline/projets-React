7b7c55704894465b46b070eafb9e7a245b305104
PalaciosCaroline <caroline2807@yahoo.fr>
2022-12-27 18:00:11 +0100

    Nettoyage

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 
 export default function Banner({ image }) {
 
-  const sectionStyle = {
-    backgroundImage: "linear-gradient(45deg,rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.3)), url(" + image + ")"
-  }
+  // const sectionStyle = {
+  //   backgroundImage: "linear-gradient(45deg,rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.3)), url(" + image + ")"
+  // }
 
+  // return (
+  //   <div className="banner" style={sectionStyle}>
+  //   </div>
+  // )
+  
   return (
-    <div className="banner" style={sectionStyle}>
+    <div className="banner" style={{ backgroundImage: `url(${image})` }}>
+      <div className="imgBanner_filter"></div>
     </div>
   )
 }
