40010e28bdfe889d44395f04a67097428a20bdf0
PalaciosCaroline <caroline2807@yahoo.fr>
2022-12-27 15:47:14 +0100

    Nettoyage

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
 export default function Banner({ image }) {
+
+  const sectionStyle = {
+    backgroundImage: "linear-gradient(rgba(0, 0, 0, 0.2),rgba(0, 0, 0, 0.2)), url(" + image + ")"
+  }
+
   return (
-    <div className="banner" style={{ backgroundImage: `url(${image})` }}>
-      <div className="imgBanner_filter"></div>
+    <div className="banner" style={sectionStyle}>
     </div>
   )
 }
