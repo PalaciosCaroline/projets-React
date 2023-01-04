27a565d63d4cfa8e06cf1a36699bd2190b86b2ae
PalaciosCaroline <caroline2807@yahoo.fr>
2023-01-04 12:40:53 +0100

    Correction

--- src/Router.jsx
+++ src/Router.jsx
@@ -10,7 +10,7 @@ export default function Router() {
       <Route index element={<Home />} />
       <Route path="/Apropos" element={<Apropos />} />
       <Route path="/logement/:id" element={<Logement />} />
-      <Route path='/retour-cv' component={() => {
+      <Route path='/retour_cv' component={() => {
     window.location.replace('https://palacioscaroline.github.io');
     return null;
 }}/>
