fc1c96d39edb6f3eb1de0e9e3b93552db9785c67
PalaciosCaroline <caroline2807@yahoo.fr>
2023-01-15 17:19:58 +0100

    rendu maquette

--- src/projet12/components/Navhorizontale.jsx
+++ src/projet12/components/Navhorizontale.jsx
@@ -4,61 +4,7 @@ import iconlogo from './../assets/logo_sportseeSVG.svg'
 import styled from 'styled-components'
 import { palette } from './../theme/styledvariable'
 
-const Header = styled.header`
-  position: relative;
-  z-index:2;
-  min-height: 91px;
-  width:100%;
-  
-`
- 
-const UlHeader = styled.ul`
-  font-size: 1.4rem;
-  // min-height: 40px;
-  line-height : 91px;
-  justify-content: space-between;
-  // align-items: center;
-  display: flex;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  background-color: ${palette.colorHeader};
-  padding: 0 8% 0 2%;
-  @media (max-width: 450px) {
-    padding: 0;
-  } 
-  a{
-    @media (max-width: 1000px) {
-      font-size: 1rem;
-      margin: 0 10px 0 10px;
-    } 
-    @media (max-width: 600px) {
-      font-size: 0.8rem;
-    } 
-    
-  }
-  
-`
 
-const Logohome = styled.a`
-  width: 178px;
-  display: block;
-  height:100%;
-  display: flex;
-  align-items: center;
-  @media (max-width: 600px) {
-    width: 60px;
-  } 
-  img{
-    @media (max-width: 600px) {
-      width:80px;
-    } 
-  }
-`
-
-const LinkHeader = styled.a`
-  color: #fff;
-  font-size: 1.5rem;
-  font-weight: 500;
-`
 
 /** render Header_page Nav
   * @param {img} iconlogo
@@ -98,3 +44,64 @@ export default function Navhorizontale() {
 Navhorizontale.propTypes = {
   img: PropTypes.any
 }
+
+
+const Header = styled.header`
+  position: relative;
+  z-index:2;
+  min-height: 91px;
+  width:100%;
+  @media (max-width: 450px) {
+    min-height: 60px;
+  } 
+  
+`
+ 
+const UlHeader = styled.ul`
+  font-size: 1.4rem;
+  // min-height: 40px;
+  line-height : 91px;
+  justify-content: space-between;
+  // align-items: center;
+  display: flex;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  background-color: ${palette.colorHeader};
+  padding: 0 8% 0 2%;
+  @media (max-width: 450px) {
+    line-height : 60px;
+    padding: 0;
+  } 
+  a{
+    @media (max-width: 1000px) {
+      font-size: 1rem;
+      margin: 0 10px 0 10px;
+    } 
+    @media (max-width: 600px) {
+      font-size: 0.8rem;
+    } 
+    
+  }
+  
+`
+
+const Logohome = styled.a`
+  width: 178px;
+  display: block;
+  height:100%;
+  display: flex;
+  align-items: center;
+  @media (max-width: 600px) {
+    width: 60px;
+  } 
+  img{
+    @media (max-width: 600px) {
+      width:80px;
+    } 
+  }
+`
+
+const LinkHeader = styled.a`
+  color: #fff;
+  font-size: 1.5rem;
+  font-weight: 500;
+`
