c95dab8ce23bfbf56615c950b4f828271a6de96c
PalaciosCaroline <caroline2807@yahoo.fr>
2023-01-17 21:00:29 +0100

    Rendu

--- src/accueilapp.jsx
+++ src/accueilapp.jsx
@@ -3,11 +3,11 @@ import { NavLink } from 'react-router-dom'
 
 export default function Accueil() {
   return (
-    <div>
-        <h1 className="h1-appWeb">Choississer le projet à visiter</h1>
+    <div id="wrapperapp">
+        <h1 >Choississer le projet à visiter</h1>
         <nav>
-            <NavLink to="/projet11" className="link-appWeb">Projet 11 site de Location d'appartement</NavLink>
-            <NavLink to="/projet12" className="link-appWeb">Projet 12 Tableau analytique avec recharts</NavLink>
+            <NavLink to="/projet11" className="link-appWeb">Projet 11<br/> site de Location d'appartement</NavLink>
+            <NavLink to="/projet12" className="link-appWeb">Projet 12<br/> Tableau analytique avec recharts</NavLink>
         </nav>
     </div>
   )
--- src/app.js
+++ src/app.js
@@ -2,26 +2,10 @@ import Router from './Router'
 
 
 export default function App() {
-  // const [loader, setLoader] = useState(true);
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     setLoader(false);
-  //   }, 800);
-  // }, []);
 
   return (
-    // loader ? (
-    //   <div className="loader_appli">
-    //   <Loader />
-    //   </div>
-    // ) : (
     <>
     <Router/>
-     {/* <h1>Choississer le projet à visiter</h1>
-
-     <NavLink to="/projet 11">Projet 11 site de Location d'appartement</NavLink>
-     <NavLink to="/projet 12">Projet 12 Tableau analytique avec recharts</NavLink> */}
     </>
     )
   // )
