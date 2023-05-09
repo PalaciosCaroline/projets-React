c350b8bee34893ca367d9e180929a0c0d45ff065
PalaciosCaroline <caroline2807@yahoo.fr>
2023-05-09 09:11:45 +0200

    update table

--- src/projet14/components/TableEmployees.jsx
+++ src/projet14/components/TableEmployees.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types';
 import { columns } from '../mocks/data';
 import {Table} from 'table-component-library';
+import {ExportDataComponent} from 'table-component-library'
 
 const EmployeeTable = React.memo(({ employees }) => {
 
   return (
     <div className='box_table'>
       <h1>List of employees</h1>
-      <Table data={employees} columns={columns}/>
+      <Table data={employees} columns={columns}
+        renderExportDataComponent={(filteredData,columnsManaged) => (
+          <ExportDataComponent
+            filteredData={filteredData} 
+            columnsManaged={columnsManaged}  
+            csvExport={true} 
+            excelExport={true} 
+            pdfExport={true} 
+          />
+        )}
+      />
     </div>
   );
 }, (prevProps, nextProps) => {
