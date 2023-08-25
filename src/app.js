880887f3d490ad1214a66edf39bcabf19a50fef5
PalaciosCaroline <caroline2807@yahoo.fr>
2023-08-25 15:17:59 +0200

    nettoyage

--- src/app.js
+++ src/app.js
@@ -1,16 +1,11 @@
 import Router from './Router'
-import { Provider } from "react-redux";
-import store from "./../src/projet14/store/index";
 import './index.css'
 
 export default function App() {
 
   return (
     <>
-    <Provider store={store}>
       <Router/>
-    </Provider>
     </>
     )
-  // )
 }
